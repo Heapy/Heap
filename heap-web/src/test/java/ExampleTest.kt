5ff469fefcf7dd2ae552d98e5a82c73c2a98ca47
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-04-22 08:53:26 +0300

    Cleanup project.

--- heap-web/src/test/java/ExampleTest.kt
+++ heap-web/src/test/java/ExampleTest.kt
@@ -3,7 +3,7 @@ import org.junit.Test
 
 class ExampleTest {
 
-    @Test(expected = java.lang.AssertionError::class)
+    @Test(expected = AssertionError::class)
     @Throws(Exception::class)
     fun `test example`() {
         assertEquals(Integer.MIN_VALUE.toLong(), (Integer.MIN_VALUE + 1).toLong())
