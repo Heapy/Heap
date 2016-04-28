aec15642c71957e4d2f2766a607a21d77b481ce1
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-04-28 14:44:30 +0300

    Usefull extension functions.

--- heap-web/src/main/kotlin/by/heap/web/Extensions.kt
+++ heap-web/src/main/kotlin/by/heap/web/Extensions.kt
@@ -3,3 +3,11 @@ package by.heap.web
 import org.slf4j.LoggerFactory
 
 inline fun <reified T : Any> logger() = LoggerFactory.getLogger(T::class.java)
+
+object L {
+    operator fun <K> get(vararg items: K) = arrayListOf(*items)
+}
+
+object A {
+    inline operator fun <reified K> get(vararg items: K) = arrayOf(*items)
+}
