644afed4853340f3aed211c5257ec1d5b4e675ba
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-05-02 14:38:54 +0300

    Sample jooq code.

--- heap-web/src/main/kotlin/by/heap/web/Application.kt
+++ heap-web/src/main/kotlin/by/heap/web/Application.kt
@@ -1,10 +1,15 @@
 package by.heap.web
 
+import by.heap.dao.jooq.tables.Films
 import by.heap.web.authentication.UserController
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
 
 
 @Configuration
@@ -12,6 +17,26 @@ import org.springframework.context.annotation.Configuration
 open class Application {
 
     @Bean open fun userController() = UserController()
+
+    @Autowired
+    lateinit var create: DSLContext;
+
+    @PostConstruct
+    fun test() {
+        var f = Films()
+
+        create.insertInto(f)
+                .set(f.CODE, "IRUS")
+                .set(f.TITLE, "NICE")
+                .set(f.DID, 42)
+                .execute()
+
+        val r = create.select(f.CODE).from(f).fetch()
+
+
+        println(r)
+    }
+
 }
 
 fun main(args: Array<String>) {
