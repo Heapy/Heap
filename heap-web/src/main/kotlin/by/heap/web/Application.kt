b14203f87c5c542c2629ee04cf4ffdd2f787cc82
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-08-09 23:59:50 +0300

    DAO example.

--- heap-web/src/main/kotlin/by/heap/web/Application.kt
+++ heap-web/src/main/kotlin/by/heap/web/Application.kt
@@ -1,9 +1,14 @@
 package by.heap.web
 
-import by.heap.dao.jooq.tables.Films
+import by.heap.dao.jooq.Tables.FILMS
+import by.heap.dao.jooq.tables.daos.FilmsDao
+import by.heap.dao.jooq.tables.pojos.Films
+import by.heap.dao.jooq.tables.records.FilmsRecord
 import by.heap.web.authentication.UserController
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
+import org.jooq.Record
+import org.jooq.RecordMapper
+import org.jooq.impl.DefaultConfiguration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
@@ -19,22 +24,34 @@ open class Application {
     @Bean open fun userController() = UserController()
 
     @Autowired
-    lateinit var create: DSLContext;
+    lateinit var config: DefaultConfiguration;
+
+    @Autowired
+    lateinit var context: DSLContext;
 
     @PostConstruct
     fun test() {
-        var f = Films()
 
-        create.insertInto(f)
-                .set(f.CODE, "IRUS")
-                .set(f.TITLE, "NICE")
-                .set(f.DID, 42)
-                .execute()
+        val dao = FilmsDao(config)
 
-        val r = create.select(f.CODE).from(f).fetch()
+        context.insertInto(FILMS)
+                .set(FILMS.CODE, "STRIN")
+                .set(FILMS.TITLE, "ABC")
+                .set(FILMS.DID, 42).execute()
 
+        // or
 
-        println(r)
+        class FilmDto(name: String)
+
+        val fooMapper: RecordMapper<FilmsRecord, Any> = RecordMapper<FilmsRecord, Any> { r: FilmsRecord -> FilmDto(r.title) }
+
+        dao.insert(Films().apply {
+
+        })
+
+        val c = dao.fetchByCode("STRIN")
+
+        println(c)
     }
 
 }
