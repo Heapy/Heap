066fea9ed43cc3e2593de92c5dabc5d2dbe1810c
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-06-15 19:20:19 +0300

    Advanced TypeScript configuration

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import styles from "./index.scss";
 
 ReactDOM.render(
   <div>
-    <div className={styles.hello_world}>Hello, World!</div>
+    <div className={styles["hello_world"]}>Hello, World!</div>
   </div>,
   document.getElementById("root")
 );
